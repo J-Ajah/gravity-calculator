43bac70d8059df6027f2ce2bbfb25e20e99a99db
J-Ajah <Justice.ajah@amalitech.org>
2021-01-17 19:46:09 +0000

    Initial commit

---
+++ src/GravityCalculator.java
@@ -0,0 +1,26 @@
+/*
+* Justice Uzoma Ajah
+* */
+
+public class GravityCalculator {
+
+
+        public static void main(String[] arguments) {
+            double gravity = -9.81; // Earth's gravity in m/s^2
+            double initialVelocity = 0.0;
+            double fallingTime = 10.0;
+            double initialPosition = 0.0;
+            double finalPosition = 0;
+
+            // Program initial output : The object's position after 10.0 secondds is 0.0 m
+
+            finalPosition = (gravity * (Math.pow(fallingTime,2)))/2 + ((initialVelocity * fallingTime) + initialPosition) ;
+
+            System.out.print("The object's position after " + fallingTime +
+                            " seconds is ");
+            System.out.printf("%.1f m. ", finalPosition );
+        }
+
+
+
+    }
